cd71b8b70e45bf0661b0598213e4f2da37bc91cf
Ricardo Martins <ricardo@ricardomartins.net.br>
2023-11-28 16:37:04 +1000

    v. 4.1.4 - Melhoria: Agora o nome do método de pagamento enviado por e-mail também mostra o meio de pagamento escolhido. Ex: "PagBank UOL - Pix"

--- rm-pagbank.php
+++ rm-pagbank.php
@@ -11,8 +11,9 @@
  * Plugin Name:       PagBank Connect
  * Plugin URI:        https://pagseguro.ricardomartins.net.br/
  * Description:       Integra seu WooCommerce com as APIs PagSeguro v4 através da aplicação de Ricardo Martins (com descontos nas taxas oficiais), com suporte a PIX transparente em muito mais.
- * Version:           4.1.3
+ * Version:           4.1.4
  * Requires at least: 5.2
+ * Tested up to:      6.4.1
  * Requires PHP:      7.4
  * Author:            martins56
  * Author URI:        https://magenteiro.com
@@ -31,7 +32,7 @@ use RM_PagBank\EnvioFacil;
 defined( 'ABSPATH' ) || die( 'No direct script access allowed!' );
 
 // Plugin constants.
-define( 'WC_PAGSEGURO_CONNECT_VERSION', '4.1.3' );
+define( 'WC_PAGSEGURO_CONNECT_VERSION', '4.1.4' );
 define( 'WC_PAGSEGURO_CONNECT_PLUGIN_FILE', __FILE__ );
 define( 'WC_PAGSEGURO_CONNECT_BASE_DIR', __DIR__ );
 define( 'WC_PAGSEGURO_CONNECT_URL', plugins_url( __FILE__ ) );
