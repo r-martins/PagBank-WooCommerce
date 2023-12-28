592b5eeb0461520703c0be71fbd0b559c287360a
Ricardo Martins <ricardo@ricardomartins.net.br>
2023-12-28 16:59:55 +1000

    v. 4.2.4 - Correção: ao escapar caracteres na versão anterior, o <strong> e <img> passaram a aparecer como textos quando desconto pix ou boleto era dado, e também no admin em pedidos com cartão.

--- rm-pagbank.php
+++ rm-pagbank.php
@@ -10,7 +10,7 @@
  * @wordpress-plugin
  * Plugin Name:       PagBank Connect
  * Description:       Integra seu WooCommerce com as APIs PagSeguro v4 através da aplicação de Ricardo Martins (com descontos nas taxas oficiais), com suporte a PIX transparente em muito mais.
- * Version:           4.2.3
+ * Version:           4.2.4
  * Requires at least: 5.2
  * Tested up to:      6.4
  * Requires PHP:      7.4
@@ -31,7 +31,7 @@ use RM_PagBank\EnvioFacil;
 defined( 'ABSPATH' ) || die( 'No direct script access allowed!' );
 
 // Plugin constants.
-define( 'WC_PAGSEGURO_CONNECT_VERSION', '4.2.3' );
+define( 'WC_PAGSEGURO_CONNECT_VERSION', '4.2.4' );
 define( 'WC_PAGSEGURO_CONNECT_PLUGIN_FILE', __FILE__ );
 define( 'WC_PAGSEGURO_CONNECT_BASE_DIR', __DIR__ );
 define( 'WC_PAGSEGURO_CONNECT_URL', plugins_url( __FILE__ ) );
