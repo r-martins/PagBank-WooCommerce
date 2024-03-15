eba64d84c81bf7504d8354e4c179b93b3b19ec8e
Ricardo Martins <ricardo@ricardomartins.net.br>
2024-03-15 10:56:37 +1000

    Correção: ao desativar e ativar novamente o plugin, um erro de SQL era gerado internamente devido a um bug no WordPress e uma mensagem de que o plugin teria gerado cerca de 1400 caracteres de saída inesperada durante a ativação era exibida no backend.

--- src/Connect.php
+++ src/Connect.php
@@ -196,8 +196,8 @@ class Connect
         
         $sql = "CREATE TABLE $recurringTable 
                 (
-                    id               int auto_increment primary key,
-                    initial_order_id int           not null comment 'Order that generated the subscription profiler',
+                    id               int auto_increment,
+                    initial_order_id int           not null UNIQUE comment 'Order that generated the subscription profiler',
                     recurring_amount float(8, 2)   not null comment 'Amount to be charged regularly',
                     status           varchar(100)  not null comment 'Current subscription status (ACTIVE, PAUSED, SUSPENDED, CANCELED)',
                     recurring_type   varchar(15)   not null comment 'Daily, Weekly, Monthly, Yearly ',
@@ -211,8 +211,7 @@ class Connect
                     suspended_reason text          null,
                     next_bill_at     datetime      not null,
                     payment_info     text          null comment 'Payment details for the subscription',
-                    CONSTRAINT wp_pagbank_recurring_unique_order_id
-                        unique (initial_order_id)
+                    PRIMARY KEY  (id)
                 )
                     comment 'Recurring profiles information for PagBank Subscribers';";
         
