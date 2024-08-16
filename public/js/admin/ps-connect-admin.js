983e53d236aee63e893550876eac2fa214d4b7c4
Ligia Salzano <ligiasalzano@gmail.com>
2024-08-16 19:50:04 -0300

    Fix admin credit card configurations js #188097195

--- public/js/admin/ps-connect-admin.js
+++ public/js/admin/ps-connect-admin.js
@@ -1,88 +1,53 @@
 jQuery(document).ready(function($) {
-
-    //region Navigation Tabs
-    jQuery('#ps-nav a').click(function(e) {
-        e.preventDefault();
-        jQuery('#ps-nav a').removeClass('nav-tab-active');
-        jQuery(this).addClass('nav-tab-active');
-        jQuery('.tab-content').hide();
-        var selected_tab = jQuery(this).attr('href');
-        jQuery(selected_tab).fadeIn();
-        //update hash
-        window.location.hash = selected_tab;
-    });
-
-    //load current tab from #
-    var hash = window.location.hash;
-    if (hash) {
-        jQuery('.nav-tab-wrapper a[href="' + hash + '"]').click();
-    }
-    //endregion
-    
-    //region Displaying and hidding general options
-    //display #woocommerce_rm-pagbank_title_display only if #woocommerce_rm-pagbank_standalone is checked
-    function hideOrShowTitleDisplay() {
-        return function () {
-            if (jQuery(this).is(':checked')) {
-                jQuery('#woocommerce_rm-pagbank_title_display').closest('tr').hide();
-            } else {
-                jQuery('#woocommerce_rm-pagbank_title_display').closest('tr').show();
-            }
-        };
-    }
-    
-    jQuery(document).on('change', '#woocommerce_rm-pagbank_standalone', hideOrShowTitleDisplay());
-    hideOrShowTitleDisplay().call(jQuery('#woocommerce_rm-pagbank_standalone'));
-    //endregion
-    
     //region Displaying and hiding credit card options
-    //display #woocommerce_rm-pagbank_cc_installment_options_fixed based on #woocommerce_rm-pagbank_cc_installment_options == fixed
+    //display #woocommerce_rm-pagbank-cc_cc_installment_options_fixed based on #woocommerce_rm-pagbank-cc_cc_installment_options == fixed
     function hideOrShowFixedOptions() {
         return function () {
             if (jQuery(this).val() === 'fixed') {
-                jQuery('#woocommerce_rm-pagbank_cc_installment_options_fixed').closest('tr').show();
+                jQuery('#woocommerce_rm-pagbank-cc_cc_installment_options_fixed').closest('tr').show();
             } else {
-                jQuery('#woocommerce_rm-pagbank_cc_installment_options_fixed').closest('tr').hide();
+                jQuery('#woocommerce_rm-pagbank-cc_cc_installment_options_fixed').closest('tr').hide();
             }
         };
     }
 
-    jQuery(document).on('change', '#woocommerce_rm-pagbank_cc_installment_options', hideOrShowFixedOptions());
-    hideOrShowFixedOptions().call(jQuery('#woocommerce_rm-pagbank_cc_installment_options'));
+    jQuery(document).on('change', '#woocommerce_rm-pagbank-cc_cc_installment_options', hideOrShowFixedOptions());
+    hideOrShowFixedOptions().call(jQuery('#woocommerce_rm-pagbank-cc_cc_installment_options'));
 
-    //display woocommerce_rm-pagbank_cc_installment_options_min_total based on #woocommerce_rm-pagbank_cc_installment_options == min_total
+    //display woocommerce_rm-pagbank-cc_cc_installment_options_min_total based on #woocommerce_rm-pagbank-cc_cc_installment_options == min_total
     function hideOrShowMinTotalOptions() {
         return function () {
             if (jQuery(this).val() === 'min_total') {
-                jQuery('#woocommerce_rm-pagbank_cc_installments_options_min_total').closest('tr').show();
+                jQuery('#woocommerce_rm-pagbank-cc_cc_installments_options_min_total').closest('tr').show();
             } else {
-                jQuery('#woocommerce_rm-pagbank_cc_installments_options_min_total').closest('tr').hide();
+                jQuery('#woocommerce_rm-pagbank-cc_cc_installments_options_min_total').closest('tr').hide();
             }
         };
     }
 
-    jQuery(document).on('change', '#woocommerce_rm-pagbank_cc_installment_options', hideOrShowMinTotalOptions());
-    hideOrShowMinTotalOptions().call(jQuery('#woocommerce_rm-pagbank_cc_installment_options'));
+    jQuery(document).on('change', '#woocommerce_rm-pagbank-cc_cc_installment_options', hideOrShowMinTotalOptions());
+    hideOrShowMinTotalOptions().call(jQuery('#woocommerce_rm-pagbank-cc_cc_installment_options'));
 
 
-    //display #woocommerce_rm-pagbank_cc_installments_options_max_installments based on #woocommerce_rm-pagbank_cc_installments_options_limit_installments == yes
+    //display #woocommerce_rm-pagbank-cc_cc_installments_options_max_installments based on #woocommerce_rm-pagbank-cc_cc_installments_options_limit_installments == yes
     function hideOrShowMasInstallmentsOptions() {
         return function () {
             if (jQuery(this).val() === 'yes') {
-                jQuery('#woocommerce_rm-pagbank_cc_installments_options_max_installments').closest('tr').show();
+                jQuery('#woocommerce_rm-pagbank-cc_cc_installments_options_max_installments').closest('tr').show();
             } else {
-                jQuery('#woocommerce_rm-pagbank_cc_installments_options_max_installments').closest('tr').hide();
+                jQuery('#woocommerce_rm-pagbank-cc_cc_installments_options_max_installments').closest('tr').hide();
             }
         };
     }
 
-    jQuery(document).on('change', '#woocommerce_rm-pagbank_cc_installments_options_limit_installments', hideOrShowMasInstallmentsOptions());
-    hideOrShowMasInstallmentsOptions().call(jQuery('#woocommerce_rm-pagbank_cc_installments_options_limit_installments'));
+    jQuery(document).on('change', '#woocommerce_rm-pagbank-cc_cc_installments_options_limit_installments', hideOrShowMasInstallmentsOptions());
+    hideOrShowMasInstallmentsOptions().call(jQuery('#woocommerce_rm-pagbank-cc_cc_installments_options_limit_installments'));
 
     //endregion
 
 	//region Showing that you are using test mode (when using a CONSANDBOX key)
-	if (jQuery('#woocommerce_rm-pagbank_connect_key').val().indexOf('CONSANDBOX') === 0){
+    var value = jQuery('#woocommerce_rm-pagbank_connect_key').val();
+	if (value && value.indexOf('CONSANDBOX') === 0){
 		//create p element
 		var p = document.createElement('p');
 		p.innerHTML = '⚠️ Você está usando o <strong>modo de testes</strong>. Veja <a href="https://dev.pagbank.uol.com.br/reference/simulador" target="_blank">documentação</a>.<br/>Para usar o modo de produção, altere suas credenciais.<br/>Lembre-se: pagamentos em Sandbox não aparecerão no PagBank, mesmo no ambiente Sandbox.';
