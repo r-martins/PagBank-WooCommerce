a8d93a360dd9934ffd8f401e0244f530ad7c588e
Ligia Salzano <ligiasalzano@gmail.com>
2024-10-23 19:49:07 -0300

    Fix get_icon. Uses the get_icon method instead of the filter because an arguments error #188450794

--- src/Traits/PaymentMethodIcon.php
+++ src/Traits/PaymentMethodIcon.php
@@ -9,19 +9,19 @@ trait PaymentMethodIcon
     /**
      * Get gateway icon
      *
-     * @param $icon
-     * @param $id
-     * @return mixed|string
+     * @return string
      */
-    public function getGatewayIcon($icon, $id) {
+    public function get_icon() {
+        $icon = parent::get_icon();
         $isDynamicIcoAccessible = Params::getIsDynamicIcoAccessible();
-        if ($isDynamicIcoAccessible && $id === $this->id) {
+        if ($isDynamicIcoAccessible) {
             $this->icon = apply_filters(
                 'wc_pagseguro_connect_icon',
                 plugins_url('public/images/payment-icon.php?method=' . $this->code, WC_PAGSEGURO_CONNECT_PLUGIN_FILE)
             );
             return '<img src="' . esc_url( \WC_HTTPS::force_https_url( $this->icon ) ) . '" alt="' . esc_attr( $this->get_title() ) . '" />';
         }
+
         return $icon;
     }
 }
