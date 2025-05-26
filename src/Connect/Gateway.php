97d2eb9ac328bd84f3291de9b64219a8bb8cbee0
Ricardo Martins <ricardo@ricardomartins.net.br>
2025-05-27 08:18:19 +1000

    Mudando msg de sandbox

--- src/Connect/Gateway.php
+++ src/Connect/Gateway.php
@@ -286,8 +286,8 @@ class Gateway extends WC_Payment_Gateway_CC
             $html .= '<div class="rm-pagbank-connect-key-info">';
             $html .= '<span class="dashicons dashicons-warning"></span>';
             $html .= '<strong>' . __('Sandbox ativo', 'pagbank-connect') . ': </strong> '
-                . __('você está testando o PagBank. Pedidos feitos neste ambiente', 'pagbank-connect')
-                . ' <u>' . __('não são reais', 'pagbank-connect') . '</u>.<br>';
+                . __('você está testando o PagBank. Pedidos feitos neste ambiente não aparecerão no PagBank.', 'pagbank-connect')
+                . '<br>';
 
             $html .= '<a href="' . esc_url('https://developer.pagbank.com.br/docs/simulador') . '" target="_blank" class="rm-pagbank-doc-link">';
             $html .= __('Documentação do Simulador', 'pagbank-connect');
