4acd4f0e245d9d3968fdec3fcb3ccd355eb36c84
Ricardo Martins <ricardo@ricardomartins.net.br>
2026-01-28 12:25:31 +1000

    Correção: em alguns cenários de race conditions, pedidos vindos de assinaturas poderiam ser gerados em duplicidade, especialmente em lojas que não configuraram o cron corretamente.

--- src/Connect.php
+++ src/Connect.php
@@ -264,6 +264,7 @@ class Connect
                 'hpos_enabled' => wc_get_container()->get(\Automattic\WooCommerce\Internal\DataStores\Orders\CustomOrdersTableController::class)->custom_orders_table_usage_is_enabled() ? 'yes' : 'no',
                 'litespeed_cache' => is_plugin_active('litespeed-cache/litespeed-cache.php') ? 'yes' : 'no',
                 'wordfence_active' => is_plugin_active('wordfence/wordfence.php') ? 'yes' : 'no',
+                'cron_ok' => defined('DISABLE_WP_CRON') && DISABLE_WP_CRON ? 'yes' : 'no',
             ],
         ];
 
